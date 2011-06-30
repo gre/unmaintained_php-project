e8ac1e2092eb57a1325ec5f88950f85f376335c7
Gaetan Renaudeau <gre@zenexity.com>
2011-06-30 11:09:47 +0200

    Add README and data.sql

---
+++ app/data.sql
@@ -0,0 +1,8 @@
+
+insert into admin (no_admin, nom_admin, login, mpadmin) values (1, 'Greg Iluvatar', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+
+insert into client (code_client, nom_client, adresse_cl, raison_sociale, identclient, mpclient, confirme) values('Dav770', 'David Oleg', '1 rue de Paris, 77000 Melun', 'MicroFrost', 'client', 'd2a04d71301a8915217dd5faf81d12cffd6cd958', true);
+
+insert into ingenieur (no_employe, nom_ing, adresse_ing, niv_salaire, categorie, login, mping) values
+(2,'Bob Linger','45 rue de Evry, 77000 Melun',1,'autre','ingenieur','f6277e16693d7a9f862699dbb7d69fef3045e0b4');
+
